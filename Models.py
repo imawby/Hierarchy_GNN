be19d8403118a19af34df4ef74f1e5e1cf58034f
isobel <isobel@isobels-MacBook-Air.local>
2024-06-10 13:07:16 +0100

    Adding model.py

---
+++ Models.py
@@ -0,0 +1,42 @@
+import torch
+from torch_geometric.nn import SAGEConv
+
+# To calculate node embeddings
+class GNN(torch.nn.Module):
+    
+    def __init__(self, node_feautres, hidden_channels):
+        super().__init__()
+        self.conv1 = SAGEConv(node_feautres, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+        
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = func.relu(x)
+        x = self.conv2(x, edge_index)
+        return x
+    
+
+# Our final classifier applies the dot-product between source and destination
+class Classifier(torch.nn.Module):
+    
+    def forward(self, x, edge_index) :
+        # Convert node embeddings to edge-level representations:
+        source_node_features = x[edge_index[0]]
+        target_node_features = x[edge_index[1]]
+        # Apply dot-product to get a prediction per supervision edge:
+        dot_product = (source_node_features * target_node_features).sum(dim=-1)
+        return torch.sigmoid(dot_product)
+
+#class Model(torch.nn.Module):
+#    
+#    def __init__(self, node_feautres, hidden_channels):
+#        super().__init__()
+#        # Instantiate homogeneous GNN:
+#        self.gnn = GNN(node_feautres, hidden_channels)
+#        self.classifier = Classifier()
+        
+#    def forward(self, x, edge_index):
+#        gnnModel = self.gnn(x, edge_index)
+#        pred = self.classifier(x, edge_index)
+#        return pred
+        
